e23913bb6dc9900d831c5c20f91cd87863351455
Sean King <sean@seanking.org>
2016-02-10 14:24:34 +1100

    added styling for optional action button

--- src/sm-ui-modal.js
+++ src/sm-ui-modal.js
@@ -5,8 +5,18 @@ class SmUiModal {
     this.is = 'sm-ui-modal';
 
     this.properties = {
+      /**
+       * Title of the modal
+       * @type {String}
+       */
       title: String,
-      hasBanner: {
+
+      /**
+       * Whether the modal has a banner
+       * Computed from title
+       * @type {Boolean}
+       */
+      _hasBanner: {
         type: Boolean,
         computed: '_computeHasBanner(title)',
         value: false
